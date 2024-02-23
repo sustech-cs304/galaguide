8ad58b750bbe5ad8c46b39304f2fd181cb47db72
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-02-23 18:30:20 +0800

    Configure dependencies.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+
+    //serialization
+    kotlin("plugin.serialization") version "1.9.22"
+
+    //ktor
+    id("io.ktor.plugin") version "2.3.8"
 }
 
 group = "org.example"
@@ -10,7 +16,31 @@ repositories {
 }
 
 dependencies {
+    //testing
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.ktor:ktor-server-test-host")
+
+    //json serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    //ktor
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-resources")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-status-pages")
+    implementation("io.ktor:ktor-server-call-logging")
+
+    //database
+    val exposedVersion = "0.41.1"
+    val h2Version = "2.2.224"
+    val hikaricpVersion = "5.0.1"
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("com.h2database:h2:$h2Version")
+    implementation("com.zaxxer:HikariCP:$hikaricpVersion")
 }
 
 tasks.test {
@@ -18,4 +48,14 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(11)
+}
+
+application {
+    mainClass.set("org.example.MainKt")
+}
+
+ktor {
+    fatJar {
+        archiveFileName.set("backend.jar")
+    }
 }
