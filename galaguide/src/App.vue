d242b83b3fd257ede847d53336d5a9a873baad00
Charley Xiao <59205189+Charley-xiao@users.noreply.github.com>
2024-02-27 18:20:02 +0800

    Beautify Home Annoucements
    Changing font to Montserrat. Slowing down the speed of auto-scrolling. Adjusting background color.

--- galaguide/src/App.vue
+++ galaguide/src/App.vue
@@ -18,12 +18,22 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a {
+  color: rgb(43, 54, 58); 
+  text-decoration: none; 
+}
+a:hover {
+  color: rgb(115, 201, 230);
+  /*text-decoration: underline;*/
 }
 </style>
--- galaguide/src/components/HomeAnnouncements.vue
+++ galaguide/src/components/HomeAnnouncements.vue
@@ -1,22 +1,17 @@
 <template>
-    <div>
+    <div class="home-announcements">
         <h1 style="text-align: center; margin-bottom: 20px;">Announcements</h1>
+        <Vue3SeamlessScroll :list="announcements" :step="0.2" :hover="true" :wheel="true" class="scroll">
+            <div class="announcement" v-for="(announcement, index) in announcements" :key="index">
+                <span class="title">
+                    <a :href="announcement.link">{{ announcement.title }}</a>
+                </span>
+                <span class="date">{{ announcement.date }}</span>
+            </div>
+        </Vue3SeamlessScroll>
     </div>
-    <Vue3SeamlessScroll 
-        :list="announcements" 
-        :step="1"
-        :hover="true"
-        :wheel="true"
-        class="scroll">
-        <div class="announcement" v-for="(announcement, index) in announcements" :key="index">
-            <span class="title">
-                <a :href="announcement.link">{{ announcement.title }}</a>
-            </span>
-            <span class="date">{{ announcement.date }}</span>
-        </div>
-    </Vue3SeamlessScroll>
 </template>
-  
+    
 <script>
 import axios from 'axios';
 import { ref } from 'vue';
@@ -27,13 +22,13 @@ export default {
     data() {
         return {
             announcements: ref([
-                { title: 'Loading...', date: '0', link: '#' },
-                { title: 'Baidu', date: '2021-01-01', link: 'https://www.baidu.com'},
-                { title: 'Google', date: '2021-01-02', link: 'https://www.google.com'},
-                { title: 'Bing', date: '2021-01-03', link: 'https://www.bing.com'},
-                { title: 'Yahoo', date: '2021-01-04', link: 'https://www.yahoo.com'},
-                { title: 'DuckDuckGo', date: '2021-01-05', link: 'https://www.duckduckgo.com'},
-                { title: 'Yandex', date: '2021-01-06', link: 'https://www.yandex.com'}
+                { title: 'Invitation to the English Corner on Feburary 24th (Saturday)', date: '2024-02-21', link: '#' },
+                { title: 'GRE General Test - Please tell us about your experience', date: '2024-01-01', link: '#' },
+                { title: 'Algorithm Design and Analysis SE2 Spring 2024: CS208 Course Info', date: '2024-01-02', link: '#' },
+                { title: '[Info Session] HEC Paris will give an info session on Thursday, Feb. 22, 2024', date: '2024-02-03', link: '#' },
+                { title: 'Upper Elementary French Spring 2024: What is the class like and what should I do?', date: '2024-01-04', link: '#' },
+                { title: 'Reminder for the Start of Classes in Spring 2024', date: '2024-01-05', link: '#' },
+                { title: 'Notice on Issuing and Implementing the Management of Study Abroad Scholarships for Undergraduates of SUSTech', date: '2024-01-06', link: '#' }
             ])
         };
     },
@@ -44,44 +39,57 @@ export default {
         fetchAnnouncements() {
             axios.get('/api/announcements')
                 .then(response => {
-                this.announcements = response.data;
-            })
+                    this.announcements = response.data;
+                })
                 .catch(error => {
-                console.error('Error fetching announcements:', error);
-            });
+                    console.error('Error fetching announcements:', error);
+                });
         }
     }
 };
 </script>
-  
+    
 <style>
+.home-announcements {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 60%;
+    height: 270px;
+    background-color: #e5e1db;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+}
+
 .scroll {
-  height: 270px;
-  width: 500px;
-  margin: 10px auto;
-  overflow: hidden;
+    height: calc(100% - 50px);
+    overflow: hidden;
 }
 
 .scroll .announcement {
-  display: flex;
-  justify-content: space-between;
-  padding: 8px;
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    padding: 8px;
+    background-color: #f9f9f9;
+    width: 90%;
+    margin-left: 5%;
+    border-radius: 8px;
+    margin-bottom: 10px;
 }
 
 .scroll .announcement:hover {
-  background-color: #ebebeb;
+    background-color: #ebebeb;
 }
 
 .title {
-  flex: 1;
-  margin-right: 20px;
+    flex: 1;
+    margin-right: 20px;
 }
 
 .date {
-  font-size: 0.9em;
-  color: #888;
+    font-size: 0.9em;
+    color: #888;
 }
 </style>
+  
