cb5804daff1e1a47e90a59b8a92841d77ad91fa2
Charley Xiao <59205189+Charley-xiao@users.noreply.github.com>
2024-04-15 22:40:16 +0800

    Update ForumHome.vue

--- galaguide/src/components/forum/ForumHome.vue
+++ galaguide/src/components/forum/ForumHome.vue
@@ -6,13 +6,13 @@ onMounted(() => {
 });
 
 const posts = ref([
-    { id: 1, title: "Animal Science Major advice?", content: "I'm a freshman and I'm thinking about majoring in Animal Science. I'm not sure if it's the right choice for me. Can anyone give me some advice?", sender: "User 1", time: "12:00" },
-    { id: 2, title: "How to get into the College of Engineering?", content: "I'm a high school student and I'm interested in majoring in Computer Science. I'm not sure how to get into the College of Engineering. Can anyone give me some advice?", sender: "User 2", time: "12:00" },
-    { id: 3, title: "SUSTech or PKU?", content: "So SUSTech and PKU both accepted me. I'm not sure which one to choose. Can anyone give me some advice?", sender: "User 3", time: "12:10" },
-    { id: 4, title: "What GRE scores do I need to get into the MSME program?", content: "Hello, to get into the Masters of Science in Mechanical Engineering program, what GRE scores do I need?", sender: "User 4", time: "12:20" },
-    { id: 5, title: "How to get into the College of Engineering?", content: "I'm a high school student and I'm interested in majoring in Computer Science. I'm not sure how to get into the College of Engineering. Can anyone give me some advice?", sender: "User 2", time: "12:00" },
-    { id: 6, title: "SUSTech or PKU?", content: "So SUSTech and PKU both accepted me. I'm not sure which one to choose. Can anyone give me some advice?", sender: "User 3", time: "12:10" },
-    { id: 7, title: "What GRE scores do I need to get into the MSME program?", content: "Hello, to get into the Masters of Science in Mechanical Engineering program, what GRE scores do I need?", sender: "User 4", time: "12:20" },
+    { id: 1, title: "Animal Science Major advice?", content: "I'm a freshman and I'm thinking about majoring in Animal Science. I'm not sure if it's the right choice for me. Can anyone give me some advice?", sender: "User 1", time: "12:00", likes: 1, tags: ["Animal Science", "Major Advice"] },
+    { id: 2, title: "How to get into the College of Engineering?", content: "I'm a high school student and I'm interested in majoring in Computer Science. I'm not sure how to get into the College of Engineering. Can anyone give me some advice?", sender: "User 2", time: "12:00", likes: 2, tags: ["College of Engineering", "Computer Science"] },
+    { id: 3, title: "SUSTech or PKU?", content: "So SUSTech and PKU both accepted me. I'm not sure which one to choose. Can anyone give me some advice?", sender: "User 3", time: "12:10", likes: 3, tags: ["SUSTech", "PKU"] },
+    { id: 4, title: "What GRE scores do I need to get into the MSME program?", content: "Hello, to get into the Masters of Science in Mechanical Engineering program, what GRE scores do I need?", sender: "User 4", time: "12:20", likes: 4, tags: ["GRE", "MSME"] },
+    { id: 5, title: "How to get into the College of Engineering?", content: "I'm a high school student and I'm interested in majoring in Computer Science. I'm not sure how to get into the College of Engineering. Can anyone give me some advice?", sender: "User 2", time: "12:00", likes: 5, tags: ["College of Engineering", "Computer Science"] },
+    { id: 6, title: "SUSTech or PKU?", content: "So SUSTech and PKU both accepted me. I'm not sure which one to choose. Can anyone give me some advice?", sender: "User 3", time: "12:10", likes: 6, tags: ["SUSTech", "PKU"] },
+    { id: 7, title: "What GRE scores do I need to get into the MSME program?", content: "Hello, to get into the Masters of Science in Mechanical Engineering program, what GRE scores do I need?", sender: "User 4", time: "12:20", likes: 7, tags: ["GRE", "MSME"] },
 ]);
 </script>
 
@@ -34,6 +34,18 @@ const posts = ref([
                 </div>
                 <p class="post-content">{{ post.content }}</p>
                 <p class="post-sender">Posted by {{ post.sender }}</p>
+                <!-- Likes -->
+                <div class="post-likes">
+                    <button>Like</button>
+                    <p>{{ post.likes }} Likes</p>
+                </div>
+                <!-- Tags -->
+                <div class="post-tags">
+                    <p>Tags:</p>
+                    <div v-for="tag in post.tags" :key="tag" class="tag">
+                        <p>{{ tag }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -136,5 +148,62 @@ const posts = ref([
 
 #posts-filter button:hover {
     background-color: #a5adba;
+}
+
+#advanced-search {
+    display: none;
+}
+
+.post-likes {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    border-radius: 10px;
+}
+
+.post-likes button {
+    height: 30px;
+    margin-right: 1%;
+    background-color: #cbe091;
+    border-radius: 10px;
+}
+
+.post-likes button:hover {
+    background-color: #a5adba;
+}
+
+.post-tags {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    border-radius: 10px;
+}
+
+.tag {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: auto;
+    height: auto;
+    background-color: #f7f1a9;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin-left: 1%;
+}
+
+.tag p {
+    font-size: 100%;
+    margin-left: 1%;
+    margin-right: 1%;
+}
+
+.tag:hover {
+    background-color: #6fd7ac;
 }
 </style>
