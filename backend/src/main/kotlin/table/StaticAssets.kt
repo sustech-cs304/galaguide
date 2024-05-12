08d05d13c169985dad0f140b850c638bd12d3da7
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-05-12 22:43:46 +0800

    Fixed StaticAsset.

--- backend/src/main/kotlin/table/StaticAssets.kt
+++ backend/src/main/kotlin/table/StaticAssets.kt
@@ -4,14 +4,14 @@ import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
 import java.util.*
 
 object StaticAssetTable : UUIDTable() {
-    val event = reference("event", EventTable)
+    val uploadTime = datetime("uploadTime").clientDefault { LocalDateTime.now() }
 }
 
 class StaticAsset(id: EntityID<UUID>) : UUIDEntity(id) {
     companion object : UUIDEntityClass<StaticAsset>(StaticAssetTable)
-
-    var event by Event referencedOn StaticAssetTable.event
 }
