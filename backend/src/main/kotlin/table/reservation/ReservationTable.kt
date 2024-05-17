1275a2ea2a003a6b2dc03dc3be261dd30ef17fa5
WinwayCome <2309813571@qq.com>
2024-05-18 07:07:46 +0800

    Reservation Fixed & Change Status

--- backend/src/main/kotlin/table/reservation/ReservationTable.kt
+++ backend/src/main/kotlin/table/reservation/ReservationTable.kt
@@ -1,5 +1,6 @@
 package galaGuide.table.reservation
 
+import galaGuide.table.EventTable
 import galaGuide.table.user.UserTable // 引入 UserTable 类
 
 import org.jetbrains.exposed.dao.LongEntity
@@ -12,20 +13,30 @@ import org.jetbrains.exposed.sql.javatime.datetime
 object ReservationTable : LongIdTable() {
     val initiatorId = reference("initiator_id", UserTable, onDelete = ReferenceOption.CASCADE)
     val recipientId = reference("recipient_id", UserTable, onDelete = ReferenceOption.CASCADE)
-    val status = varchar("status", 20) // 添加订单状态字段
+    val status = enumerationByName("status", 20, OrderStatus::class)
     val price = decimal("price", 10, 2)
     val createTime = datetime("create_time")
-    val activityId = long("activity_id")
-    val sessionId = long("session_id")
+    val eventId = reference("event_id", EventTable, onDelete = ReferenceOption.CASCADE)
+    val periodId = reference("period_id", EventTable, onDelete = ReferenceOption.CASCADE)
 }
 
 class Order(id: EntityID<Long>) : LongEntity(id) {
     companion object : LongEntityClass<Order>(ReservationTable)
+
     var initiatorId by ReservationTable.initiatorId
     var recipientId by ReservationTable.recipientId
     var status by ReservationTable.status
     var price by ReservationTable.price
     var createTime by ReservationTable.createTime
-    var activityId by ReservationTable.activityId
-    var sessionId by ReservationTable.sessionId
+    var eventId by ReservationTable.eventId
+    var periodId by ReservationTable.periodId
+}
+
+enum class OrderStatus {
+    NONPAYMENT,
+    CONFIRMED,
+    USED,
+    REFUNDING,
+    CLOSED,
+    ERROR
 }
