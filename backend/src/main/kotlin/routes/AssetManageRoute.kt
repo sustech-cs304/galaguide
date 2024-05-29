f66fb3cb6c509d4ceb4bef23f8ffedb0d26fdba9
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-05-29 13:18:51 +0800

    Fixed get asset need login.

--- backend/src/main/kotlin/routes/AssetManageRoute.kt
+++ backend/src/main/kotlin/routes/AssetManageRoute.kt
@@ -20,7 +20,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import java.io.InputStream
 import java.util.*
 
-fun Route.routeAssetManage() = authenticate("user") {
+fun Route.routeAssetManage() {
     route("/asset") {
         val logger = KtorSimpleLogger(StaticAssetManager::class.qualifiedName!!)
 
