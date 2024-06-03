4b92274e5f7356aa142af4db1a941a7fb9f5bc8a
WinwayCome <2309813571@qq.com>
2024-06-04 02:08:13 +0800

    Fixed ForumRoute

--- backend/src/main/kotlin/data/ForumResponses.kt
+++ backend/src/main/kotlin/data/ForumResponses.kt
@@ -1,27 +1,30 @@
 package galaGuide.data
 
 import galaGuide.table.forum.Discuss
+import galaGuide.table.forum.TagTable
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class DiscussDetail(
+    val id: Long,
     val title: String = "",
     val content: String? = null,
     val posterId: Long? = null,
     val posterName: String? = null,
     val likes: Long = 0,
     val time: Long? = null,
-    val belongsToId: Long
+    val belongsToId: Long,
 )
 
 fun Discuss.asDetail() = DiscussDetail(
+    id.value,
     title,
     content,
     poster.id.value,
     poster.name,
     likes,
     createTime.epochSecond,
-    belongsToId
+    belongsToId,
 )
 
 fun Discuss.asRestResponse() = asDetail().asRestResponse()
