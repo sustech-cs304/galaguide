353889bbf1a4f5779fdd510bd1d5365f6132570c
Charley Xiao <59205189+Charley-xiao@users.noreply.github.com>
2024-06-05 20:17:37 +0800

    Delete initiate.bat

--- initiate.bat
+++
@@ -1,9 +0,0 @@
-% Start Frontend %
-start "galaguide" cmd /k "cd .\galaguide\ & npm run electron:serve"
-
-@REM Start Backend
-cd .\pseudo\
-python app.py
-
-@REM cd .\backend
-@REM ./gradlew.bat runFatJar
